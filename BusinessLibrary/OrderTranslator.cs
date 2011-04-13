8e599a36b52404e8f92bebc0eb161c65a5472c67
Frank Mao <maosd2@yahoo.com>
2011-04-13 14:15:02 -0600

    added missing unit-test

---
+++ BusinessLibrary/OrderTranslator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using Core.Models;
+
+namespace BusinessLibrary
+{
+    public class OrderTranslator  
+    {
+        public OrderTranslator()
+        {
+            Mapper.CreateMap<OrderModel, BusinessLibrary.TweekedCsla.Order>();
+            Mapper.CreateMap<LineItemModel, LineItem>();
+        }
+
+        public TweekedCsla.Order From(OrderModel orderModel)
+        {
+            return Mapper.Map<OrderModel, TweekedCsla.Order>(orderModel);
+        }
+    }
+}
---
+++ Test/UnitTests/TranslatorSpecs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BusinessLibrary;
+using Core.Models;
+using NUnit.Framework;
+
+namespace Test.UnitTests
+{
+    [TestFixture]
+    class TranslatorSpecs
+    {
+        [Test]
+        public void ShouldMapRootPropertiesWithChildList()
+        {
+            var orderModel = new OrderModel(){CustomerName = "A customer"
+
+                                             };
+            orderModel.AddLineItem(new LineItemModel(){Name = "Linteitme1"});
+            var result = new OrderTranslator().From(orderModel);
+
+            Assert.AreEqual(orderModel.CustomerName, result.CustomerName);
+            Assert.Greater(result.LineItems.Count, 0);
+            Assert.AreEqual(result.LineItems[0].Name, "Linteitme1");
+
+            Console.WriteLine(result.CustomerName);
+            Console.WriteLine(result.LineItems.Count);
+        }
+    }
+
+
+}
