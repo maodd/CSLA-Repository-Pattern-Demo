6d3a0882387a617abdd06a71067ecc8f7cba2588
Frank Mao <Frank Mao>
2011-04-21 14:44:06 -0600

    add developwithpassion.specifications.demo

---
+++ Specs/DevelopWithPassion.Specifications.Demo/OrderTranslatorSpecs.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLibrary;
+using Core.Models;
+using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
+using Order = BusinessLibrary.TweekedCsla.Order;
+
+namespace DevelopWithPassion.Specifications.Demo
+{
+    public class OrderTranslatorSpecs
+    {
+
+
+        [Subject(typeof(OrderTranslator))]
+        public class When_translate_NHibernate_model_to_csla_bo : Observes<OrderTranslator>
+        {
+            static Order result;
+
+            Because b = () =>
+                {
+                    var orderModel = new OrderModel
+                                         {
+                                             CustomerName = "A customer"
+                                         };
+                    orderModel.AddLineItem(new LineItemModel { Name = "Linteitme1" });
+
+                    result = sut.From(orderModel);
+                };
+
+            It should_return_valid_csla_bo = () =>
+                {
+                    result.IsValid.ShouldBeTrue();
+                    result.CustomerName.ShouldEqual("A customer");
+                    result.LineItems.Count.ShouldEqual(1);
+                    result.IsDirty.ShouldBeFalse();
+                    result.IsNew.ShouldBeFalse();
+                };
+        }
+    }
+}
